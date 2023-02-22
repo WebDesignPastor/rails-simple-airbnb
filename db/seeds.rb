8cf76bbfb77804a2a37ca4d20c72b9c7e449f77e
Francois PARMENTIER$ <parmentier.corporate@gmail.com>
2023-02-22 17:03:01 +0100

    airbnb like done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Flat.create!([{
+  name: 'usine désaffectée',
+  address: 'fond de rue',
+  description: 'Des fenêtres grandes mais un chauffage central en mauvais état',
+  price_per_night: 75,
+  number_of_guests: 3
+},
+{
+  name: 'Vieille chambre claquée au sol',
+  address: 'Passage merguez',
+  description: "Vous n'aurez qu'une lucarne pour voir la lumière du jour",
+  price_per_night: 125,
+  number_of_guests: 9
+},
+{
+  name: 'Manoir délabré',
+  address: 'Fond du derch costarmoricain',
+  description: 'Vous rentrez, vous ne ressortez jamais',
+  price_per_night: 240,
+  number_of_guests: 16
+},
+{
+  name: 'Maison close normande',
+  address: 'Bord de falaise',
+  description: 'Spacieux mais bondé',
+  price_per_night: 30,
+  number_of_guests: 45
+},]
+)
